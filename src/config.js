b0e4261d32e80e841dd86080f88544f0185f903d
Rainey <515385036@qq.com>
2017-03-15 20:05:34 +0800

    delete config

--- src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    dbLink: 'mongodb://127.0.0.1:27017/rainey',
-    keys: ['SyJ9Jn1IWzLlNueGVYG']
-};
--- test/solt.js
+++ test/solt.js
@@ -1,8 +1,20 @@
 const bcrypt = require('bcrypt');
-const SALT_ROUNDS = 10;
-const salt = bcrypt.genSaltSync(SALT_ROUNDS);
-const password = '12345678';
-const safePassword = bcrypt.hashSync(password, salt);
-console.log(safePassword);
-const is = bcrypt.compareSync('password', safePassword);
-console.log(is);
+// const SALT_ROUNDS = 1;
+
+const func = (SALT_ROUNDS) => {
+    const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+    const password = '12345678';
+    const safePassword = bcrypt.hashSync(password, salt, 1);
+    return safePassword;
+    // console.log('salt::', salt);
+    // console.log('safePassword::', safePassword);
+}
+const safePassword = func(12);
+
+const start = Date.now();
+for(let i = 0; i< 100; i++) {
+    console.log('rounds::', i);
+    bcrypt.compareSync('password', safePassword)
+}
+console.log('tiemout::', Date.now() - start, 'ms');
+  
